d56374a1556eae1cf722134ba72f8bbb20a70cf1
TrulyGoner <166147307+TrulyGoner@users.noreply.github.com>
2024-11-15 17:06:01 +0300

    Update TodoWrapper.js

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,9 +1,7 @@
 import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
-import { v4 as uuidv4 } from "uuid";
 import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
-uuidv4();
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([])
