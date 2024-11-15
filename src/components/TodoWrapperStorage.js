d671c45b5faa981e6184156baa164a476556d267
TrulyGoner <166147307+TrulyGoner@users.noreply.github.com>
2024-11-15 17:03:12 +0300

    Update TodoWrapperStorage.js

--- src/components/TodoWrapperStorage.js
+++ src/components/TodoWrapperStorage.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { TodoForm } from './TodoForm'
-import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
-uuidv4();
 
 export const TodoWrapperLocalStorage = () => {
     const [todos, setTodos] = useState([])
